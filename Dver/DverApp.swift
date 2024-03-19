7c69a4a83b523b6fd399cc5c1162b3c7f80b829d
Alex <alex04gilabert@gmail.com>
2024-03-19 15:21:23 -0400

    Initial Commit

---
+++ Dver/DverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DverApp.swift
+//  Dver
+//
+//  Created by Maria Jose rivas ruiz on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
